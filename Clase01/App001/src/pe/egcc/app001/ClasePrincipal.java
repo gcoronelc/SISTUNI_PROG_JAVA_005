abd6e960d0351f31df20141d89c8e87f5f3e21d0
Gustavo Coronel <gcoronelc@gmail.com>
2016-04-23 18:58:42 -0500

    Clase 01

---
+++ Clase01/App001/src/pe/egcc/app001/ClasePrincipal.java
@@ -0,0 +1,15 @@
+package pe.egcc.app001;
+
+import pe.egcc.app001.view.SumarView;
+
+/**
+ *
+ * @author Gustavo Coronel
+ */
+public class ClasePrincipal {
+
+  public static void main(String[] args) {
+    SumarView.main(null);
+  }
+
+}
